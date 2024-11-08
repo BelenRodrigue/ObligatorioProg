3ad21703363ce0ec75781f3f6ed447f4581ed782
BelenRodrigue <brodriguez4@correo.um.edu.uy>
2024-11-08 20:35:31 -0300

    registrar aventurero en el gremio

--- entities/gremio.py
+++ entities/gremio.py
@@ -1,3 +1,6 @@
+from entities.aventurero import Aventurero
+from exceptions.valorInvalido import ValorInvalido
+
 
 class Gremio:
     def __init__(self, aventureros: list, misiones: list):
@@ -12,4 +15,10 @@ class Gremio:
     def misiones(self):
         return self.__misiones
     
-   
+    def registrar_aventurero_en_el_gremio(self, nombre, clase, atributo_adicional, ID):
+        
+        for iter_aventurero in self.__aventureros:
+            if type(iter_aventurero) is Aventurero and iter_aventurero.ID == ID:
+                raise ValorInvalido
+            
+            
