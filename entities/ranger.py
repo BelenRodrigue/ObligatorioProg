b4b3ac31b4b45e745107de288d4e2d0f32b58f58
BelenRodrigue <brodriguez4@correo.um.edu.uy>
2024-11-08 19:31:18 -0300

    clases casi terminadas

--- entities/ranger.py
+++ entities/ranger.py
@@ -3,7 +3,7 @@ from entities.aventurero import Aventurero
 class Ranger(Aventurero):
     def __init__(self, nombre: str, ID: int, puntos_de_habilidad: int, experiencia: int, dinero: float, mascota: bool):
         super().__init__(nombre, ID, puntos_de_habilidad, experiencia, dinero)
-        self.__mascota = bool 
+        self.__mascota = mascota 
 
     @property
     def mascota(self):
